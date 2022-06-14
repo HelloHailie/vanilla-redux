54c2cebb1d9b2d2391b514aeee5829664c6d1eb5
HelloHailie <alsltjsals@gmail.com>
2022-06-14 23:17:26 +0900

    0614

--- Documents/GitHub/react-complete-guide/src/App.js
+++ Documents/GitHub/react-complete-guide/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './component/ExpenseItem';
 
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem />
     </div>
   );
 }
